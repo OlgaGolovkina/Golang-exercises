8d6cbca83970a0c4b577b2afa0348f97d2df2e5f
Olga Golovkina <olgadietology@gmail.com>
2017-07-24 21:22:17 +0300

    Create the function that search the max number

--- max.go
+++ max.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(max_number(1, 2, 3, 4, 5))
+}
+
+func max_number(args ...int) int {
+	var maxNumber int = 0
+	for _, number := range args {
+		if number > maxNumber {
+			maxNumber = number
+		}
+	}
+	return maxNumber
+}
