d1fdb8a875c473e1a31e3749d5b041d8f900ddf6
Olga Golovkina <olgadietology@gmail.com>
2017-07-25 09:05:18 +0300

    Usage of the interface

--- interface.go
+++ interface.go
@@ -6,13 +6,24 @@ type Square struct {
 	Side int
 }
 
-func(s *Square) Area() (area int)  {
+func(s Square) Area() (area int)  {
 	area = s.Side * s.Side
 	return
+}
+
+type Shape interface {
+	Area() int
+}
+
+func info(s Shape) {
+	fmt.Println(s)
+	fmt.Println(s.Area())
 }
 
 func main()  {
 	side := Square{5}
 	fmt.Println(side)
 	fmt.Println(side.Area())
+
+	info(side)
 }
